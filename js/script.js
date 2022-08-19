ffdcea75b250d668bf066dcb8a07443c16e10448
mark <markpackham1@gmail.com>
2022-08-19 10:53:28 +0100

    add countUp function
    https://youtu.be/wryPX7KSwSc?t=6563

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
 const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
 
 btn.addEventListener("click", navToggle);
 
@@ -10,3 +11,16 @@ function navToggle() {
   document.body.classList.toggle("stop-scrolling");
   menu.classList.toggle("show-menu");
 }
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      // convert to a number using "+" could have used parseInt() instead
+      const target = +counter.getAttribute("data-target");
+    };
+  });
+}
+
+countUp();
